b7854691dc5068bbe375386cb6353f78ae38c523
Enzo Lima <enzo2006lima@gmail.com>
2026-07-08 18:11:51 -0300

    tiposPrimitivos

--- src/VarPrimitivas.java
+++ src/VarPrimitivas.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class VarPrimitivas {
 
     public static void main(String[] args){
 
+        Scanner teclado = new Scanner(System.in);
+
+        int valor = 3;
+        float nota = 7.4537f;
+        char letra = 'E';
+        String nome = teclado.nextLine();
+
+        System.out.printf("\nA nota do aluno %s é %.4f\n", nome, nota);
     }
 
 }
